14632bea4dbff05296eddd029773df581fded5aa
Toms Veidemanis <toms@chess24.com>
2021-06-01 19:52:48 +0300

    FEAT: add query caching

--- apps/rickmorty/pages/_app.tsx
+++ apps/rickmorty/pages/_app.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Hydrate } from 'react-query/hydration';
 import { ChakraProvider } from '@chakra-ui/react';
 
-import { Box } from '@chakra-ui/react'
-
-const queryClient = new QueryClient()
+import { Box } from '@chakra-ui/react';
 
 function CustomApp({ Component, pageProps }: AppProps) {
+  const queryClientRef = React.useRef<QueryClient>();
+
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
+
   return (
     <ChakraProvider>
-      <QueryClientProvider client={queryClient}>
-        <Box>Header</Box>
-        <Component {...pageProps} />
+      <QueryClientProvider client={queryClientRef.current}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Box>Header</Box>
+          <Component {...pageProps} />
+        </Hydrate>
       </QueryClientProvider>
     </ChakraProvider>
   );
